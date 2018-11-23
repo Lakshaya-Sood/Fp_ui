208134f6fdec9be5512cac89a377c358bd987261
Lakshaya-Sood <youcandoit10@zoho.com>
2018-11-23 17:36:35 +0530

    login ui inital commit

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App';
+import Login from './components/Login/Login';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Login/>, document.getElementById('root'));
 
